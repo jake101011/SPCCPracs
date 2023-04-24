90596cfdc9f198b028b49c701e420ab579290097
jake101011 <131685180+jake101011@users.noreply.github.com>
2023-04-24 19:47:51 +0530

    Add files via upload

---
+++ printf.c
@@ -0,0 +1,15 @@
+#inlude<stdio.h>
+int main(){
+    float num1;
+    float num2;
+
+    printf("Enter a number: ");
+    scanf("%f",&num1);
+    printf("Enter another number: ");
+    scanf("%f",&num2);
+
+    printf("num1 = %f\n",num1);
+    printf("num2 = %f\n",num2);
+
+    return 0;
+}
